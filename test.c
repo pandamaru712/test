62b0ce5b57f634a64c49a295b4028cb9b19ac132
Naoto Iida <iida@h83.153.228.10.30824.vlan.kuins.net>
2016-01-30 02:22:29 +0900

    arg test.

--- test.c
+++ test.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-int main(){
-	printf("Hello\n");
-	printf("Success push!");
-	printf("test");
-	printf("common");
+int main(int argc, int *argv[]){
+	printf("argc=%d\n", argc);
+	printf("argv[2]=%s\n", argv[2]);
+	printf("argv[3]=%d\n", argv[3]);
 
 	return 0;
 }
